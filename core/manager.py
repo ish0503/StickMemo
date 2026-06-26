782804ba43e9c8a76e02c93c07cb08c25731bccd
ish0503 <98135486+ish0503@users.noreply.github.com>
2026-06-27 00:16:03 +0900

    Refactor NoteManager for improved data handling
    Refactor NoteManager to use a single data source for notes and streamline note creation and deletion.

--- core/manager.py
+++ core/manager.py
@@ -7,12 +7,16 @@ class NoteManager:
 
     def __init__(self):
         self.windows = []
-        self.notes = Storage.load_notes()
+        self.data = Storage.load()
 
-    def create_note(self, data=None):
+    def load_all(self):
+        for d in self.data:
+            self.create(d)
+
+    def create(self, data=None):
         note_id = int(time.time() * 1000)
 
-        note_data = data or {
+        d = data or {
             "id": note_id,
             "text": "",
             "x": 200,
@@ -21,19 +25,15 @@ class NoteManager:
             "height": 250
         }
 
-        window = NoteWindow(note_id=note_id, data=note_data, manager=self)
-
-        self.windows.append(window)
-        window.show()
+        win = NoteWindow(d, self)
+        self.windows.append(win)
+        win.show()
 
         self.save()
 
-    def delete_note(self, note_id):
+    def delete(self, note_id):
         self.windows = [w for w in self.windows if w.note_id != note_id]
-
-        self.notes = [n for n in self.notes if n["id"] != note_id]
-
-        Storage.save_notes(self.notes)
+        self.save()
 
     def save(self):
         data = []
@@ -48,8 +48,4 @@ class NoteManager:
                 "height": w.height()
             })
 
-        Storage.save_notes(data)
-
-    def load_all(self):
-        for note in self.notes:
-            self.create_note(note)
+        Storage.save(data)
